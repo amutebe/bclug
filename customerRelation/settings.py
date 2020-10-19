19f0a75a2f4613364a71146e871bed95181e5535
amutebe <mutebe2@gmail.com>
2020-10-19 08:49:24 +0300

    oct19final

--- customerRelation/settings.py
+++ customerRelation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^ierzn^w=%e)oyzw_ecfb_usv&#1y^80ffnxlo&aod1)!o7-9a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['andysystems.pythonanywhere.com']
 
 
 # Application definition
